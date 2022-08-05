5d4f13faf5d3f2113589680753362df4aaa96dc5
boopidoop <daniel.summerside@yahoo.com>
2022-08-05 12:30:32 +0100

    a

---
+++ APITestProject/APITestProjectApp/Services/IService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameDaysAPIFramework.Services;
+
+public interface IService
+{
+    #region Properties
+    public CallManager CallManager { get; set; }
+    public DTO<NamedayResponse> NamedayTodayDTO { get; set; }
+    public string Response { get; set; }
+    public JObject? JsonResponse { get; set; }
+    #endregion
+
+    public Task MakeRequest(Dictionary<string, string> parameters, Method method);
+    public Task MakeRequest(Method method);
+    public int GetStatusCode();
+    public string GetHeader(string headerName);
+}
