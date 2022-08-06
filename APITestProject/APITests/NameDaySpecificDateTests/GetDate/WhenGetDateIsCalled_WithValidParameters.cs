100ce21310752717a8cb11cb713782207a7c5316
kaichan1 <punkai.chan@gmail.com>
2022-08-06 22:41:26 +0100

    Test cases for NamedayByName and NamedayByDate completed

--- APITestProject/APITests/NameDaySpecificDateTests/GetDate/WhenGetDateIsCalled_WithValidParameters.cs
+++ APITestProject/APITests/NameDaySpecificDateTests/GetDate/WhenGetDateIsCalled_WithValidParameters.cs
@@ -8,5 +8,73 @@ namespace APITests.NameDaySpecificDateTests.GetDate
 {
     public class WhenGetDateIsCalled_WithValidParameters
     {
+        NamedayForDateService _namedayForDateService;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _namedayForDateService = new NamedayForDateService();
+        }
+
+        [Category("Valid Day and Month")]
+        [Test]
+        public async Task GivenValidDayAndMonth_GetDate_ReturnsStatusCode200()
+        {
+            var param = new Dictionary<string, string> { { "day", "29" }, { "month", "2" } };
+            await _namedayForDateService.MakeRequest(param, Method.Get);
+            Assert.That(_namedayForDateService.GetStatusCode(), Is.EqualTo(200));
+        }
+        [Test]
+        public async Task GivenValidDayAndMonth_GetDate_ReturnsExpectedDay()
+        {
+            var param = new Dictionary<string, string> { { "day", "29" }, { "month", "2" } };
+            await _namedayForDateService.MakeRequest(param, Method.Get);
+            Assert.That(_namedayForDateService.JsonResponse["day"].ToString(), Is.EqualTo("29"));
+        }
+        [Test]
+        public async Task GivenValidDayAndMonth_GetDate_ReturnsExpectedMonth()
+        {
+            var param = new Dictionary<string, string> { { "day", "29" }, { "month", "2" } };
+            await _namedayForDateService.MakeRequest(param, Method.Get);
+            Assert.That(_namedayForDateService.JsonResponse["month"].ToString(), Is.EqualTo("2"));
+        }
+        [Test]
+        public async Task GivenValidDayAndMonth_GetDate_ReturnsNamedays()
+        {
+            var param = new Dictionary<string, string> { { "day", "29" }, { "month", "2" } };
+            await _namedayForDateService.MakeRequest(param, Method.Get);
+            Assert.That(_namedayForDateService.JsonResponse["nameday"], Does.ContainKey("us"));
+        }
+
+
+
+        [Category("Valid Day, Month, and Country")]
+        public async Task GivenValidDayMonthAndCountry_GetDate_ReturnsStatusCode200()
+        {
+            var param = new Dictionary<string, string> { { "day", "29" }, { "month", "2" }, { "country", "us" } };
+            await _namedayForDateService.MakeRequest(param, Method.Get);
+            Assert.That(_namedayForDateService.GetStatusCode(), Is.EqualTo(200));
+        }
+        [Test]
+        public async Task GivenValidDayMonthAndCountry_GetDate_ReturnsExpectedDay()
+        {
+            var param = new Dictionary<string, string> { { "day", "29" }, { "month", "2" }, { "country", "us" } };
+            await _namedayForDateService.MakeRequest(param, Method.Get);
+            Assert.That(_namedayForDateService.JsonResponse["day"].ToString(), Is.EqualTo("29"));
+        }
+        [Test]
+        public async Task GivenValidDayMonthAndCountry_GetDate_ReturnsExpectedMonth()
+        {
+            var param = new Dictionary<string, string> { { "day", "29" }, { "month", "2" }, { "country", "us" } };
+            await _namedayForDateService.MakeRequest(param, Method.Get);
+            Assert.That(_namedayForDateService.JsonResponse["month"].ToString(), Is.EqualTo("2"));
+        }
+        [Test]
+        public async Task GivenValidDayMonthAndCountry_GetDate_ReturnsExpectedNamedays()
+        {
+            var param = new Dictionary<string, string> { { "day", "29" }, { "month", "2" }, { "country", "us" } };
+            await _namedayForDateService.MakeRequest(param, Method.Get);
+            Assert.That(_namedayForDateService.JsonResponse["nameday"], Does.ContainKey("us"));
+        }
     }
 }
